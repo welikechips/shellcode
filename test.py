57b14fa933190442a2ac9f0f0fc0d034b23d7ea2
root <root@localhost.localdomain>
2018-07-06 00:02:11 -0400

    Need to fix ghetto phpfile write

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import fileinput
-
-# Does a list of files, and
-# redirects STDOUT to the file in question
-for line in fileinput.input('php-reverse-shell.php', inplace = 1): 
-      print line.replace("127.0.0.1", "bar"),
