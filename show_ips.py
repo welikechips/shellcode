4ae21f5575929c6369e37daa7dfa31ab618522d4
Thomas Norberg <tnorberg@thomasnorberg.com>
2020-03-26 15:53:20 -0400

    text changes.

--- show_ips.py
+++ show_ips.py
@@ -7,5 +7,5 @@ def show_ips():
         addresses = [i['addr'] for i in ifaddresses(iface_name).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
         if "No IP addr" not in addresses:
             my_ips[iface_name] = ', '.join(addresses)
-    print("\nIP address on this computer:")
+    print("\nIP addresses on this computer:")
     print("\n".join(": ".join(_) for _ in sorted(my_ips.items())))
